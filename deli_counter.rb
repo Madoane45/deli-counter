9a3b5788320328162027339bba4ab95e8aa220bb
Madoane45 <Madoane45@gmail.com>
2021-02-06 14:51:46 -0500

    commit tree 43ae905d059796963607e7d9c4dfeddf0c155852

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
+require "pry"
 # Write your code here.
+ def line (in_line)
+    #binding.pry
+    if in_line.length == 0 
+          puts "The line is currently empty."
+
+    else 
+        currently = "The line is currently:"
+        
+        in_line.each.with_index(1) do |names, index|
+            currently << " #{index}. #{names}"
+            
+        end
+        puts currently
+    end    
+ end
+  
+ def take_a_number (array, string)
+    #binding.pry
+     array << string
+        puts "Welcome, #{string}. You are number #{array.length} in line." 
+    
+ end
+ 
+ def now_serving (array)
+    if array.empty? 
+        puts "There is nobody waiting to be served!"
+
+    else 
+        puts "Currently serving #{array[0]}."
+         array.shift
+    end
+ end
